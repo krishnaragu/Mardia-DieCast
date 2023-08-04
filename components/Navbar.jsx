b9052dce9372bc5e1275fc08470347e9490b4cab
Tahmeerpasha <tahmeerpashacjc@gmail.com>
2023-08-04 20:56:48 +0530

    added handleNav to navbar in mobile view

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -34,59 +34,59 @@ const Navbar = () => {
             {navOpen && (
                 <div className="absolute  top-16 right-0 mt-2 w-full bg-black p-3 z-50 shadow-lg">
                     <ul className="flex flex-col space-y-2 ">
-                        <li className='p-5' onClick={handleNav}>
-                            <Link to="home" spy={true} smooth={true} className="text-white">Home</Link>
+                        <li className='p-5' >
+                            <Link to="home" onClick={handleNav} spy={true} smooth={true} className="text-white">Home</Link>
                         </li>
-                        <li className='p-5' onClick={handleNav}>
-                            <Link to="about" spy={true} smooth={true} className="text-white">About</Link>
+                        <li className='p-5' >
+                            <Link to="about" onClick={handleNav} spy={true} smooth={true} className="text-white">About</Link>
                         </li>
-                        <li className='p-5' onClick={handleNav}>
-                            <Link to="clientele" spy={true} smooth={true} className="text-white">Clientele</Link>
+                        <li className='p-5' >
+                            <Link to="clientele" onClick={handleNav} spy={true} smooth={true} className="text-white">Clientele</Link>
                         </li>
-                        <li className='p-5' onClick={handleNav}>
-                            <Link to="products" spy={true} smooth={true} className="text-white">Products</Link>
+                        <li className='p-5' >
+                            <Link to="products" onClick={handleNav} spy={true} smooth={true} className="text-white">Products</Link>
                         </li>
-                        <li className='p-5' onClick={handleNav}>
-                            <Link to="quality" spy={true} smooth={true} className="text-white">Quality Assurance</Link>
+                        <li className='p-5' >
+                            <Link to="quality" onClick={handleNav} spy={true} smooth={true} className="text-white">Quality Assurance</Link>
                         </li>
-                        <li className='p-5' onClick={handleNav}>
-                            <Link to="infra" spy={true} smooth={true} className="text-white">Infrastructure</Link>
+                        <li className='p-5' >
+                            <Link to="infra" onClick={handleNav} spy={true} smooth={true} className="text-white">Infrastructure</Link>
                         </li>
-                        <li className='p-5' onClick={handleNav}>
-                            <Link to="whyus" spy={true} smooth={true} className="text-white">Why us?</Link>
+                        <li className='p-5' >
+                            <Link to="whyus" onClick={handleNav} spy={true} smooth={true} className="text-white">Why us?</Link>
                         </li>
 
-                        <li className='p-5' onClick={handleNav}>
-                            <Link to="contact" spy={true} smooth={true} className="text-white">Contact</Link>
+                        <li className='p-5' >
+                            <Link to="contact" onClick={handleNav} spy={true} smooth={true} className="text-white">Contact</Link>
                         </li>
                     </ul>
                 </div>
             )}
             <section id="links" className='hidden lg:block'>
                 <ul className="flex">
-                    <li onClick={handleNav} className='p-5 hover:bg-red-500 hover:cursor-pointer text-white hover:underline hover:rounded-xl'>
+                    <li className='p-5 hover:bg-red-500 hover:cursor-pointer text-white hover:underline hover:rounded-xl'>
                         <Link to="home" spy={true} smooth={true} className="">Home</Link>
                     </li>
-                    <li onClick={handleNav} className='p-5  hover:bg-red-500 hover:cursor-pointer text-white hover:underline hover:rounded-xl'>
+                    <li className='p-5  hover:bg-red-500 hover:cursor-pointer text-white hover:underline hover:rounded-xl'>
                         <Link to="about" spy={true} smooth={true} className="">About</Link>
                     </li>
-                    <li onClick={handleNav} className='p-5  hover:bg-red-500 hover:cursor-pointer text-white hover:underline hover:rounded-xl'>
+                    <li className='p-5  hover:bg-red-500 hover:cursor-pointer text-white hover:underline hover:rounded-xl'>
                         <Link to="clientele" spy={true} smooth={true} className="">Clientele</Link>
                     </li>
-                    <li onClick={handleNav} className='p-5  hover:bg-red-500 hover:cursor-pointer text-white hover:underline hover:rounded-xl'>
+                    <li className='p-5  hover:bg-red-500 hover:cursor-pointer text-white hover:underline hover:rounded-xl'>
                         <Link to="products" spy={true} smooth={true} className="">Products</Link>
                     </li>
-                    <li onClick={handleNav} className='p-5  hover:bg-red-500 hover:cursor-pointer text-white hover:underline hover:rounded-xl'>
+                    <li className='p-5  hover:bg-red-500 hover:cursor-pointer text-white hover:underline hover:rounded-xl'>
                         <Link to="quality" spy={true} smooth={true} className="">Quality Assurance</Link>
                     </li>
-                    <li onClick={handleNav} className='p-5  hover:bg-red-500 hover:cursor-pointer text-white hover:underline hover:rounded-xl'>
+                    <li className='p-5  hover:bg-red-500 hover:cursor-pointer text-white hover:underline hover:rounded-xl'>
                         <Link to="infra" spy={true} smooth={true} className="">Infrastructure</Link>
                     </li>
-                    <li onClick={handleNav} className='p-5  hover:bg-red-500 hover:cursor-pointer text-white hover:underline hover:rounded-xl'>
+                    <li className='p-5  hover:bg-red-500 hover:cursor-pointer text-white hover:underline hover:rounded-xl'>
                         <Link to="whyus" spy={true} smooth={true} className="">Why us?</Link>
                     </li>
 
-                    <li onClick={handleNav} className='p-5  hover:bg-red-500 hover:cursor-pointer text-white hover:underline hover:rounded-xl'>
+                    <li className='p-5  hover:bg-red-500 hover:cursor-pointer text-white hover:underline hover:rounded-xl'>
                         <Link to="contact" spy={true} smooth={true} className="">Contact</Link>
                     </li>
                 </ul>
