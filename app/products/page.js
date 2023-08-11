39fabd5c77079db633a0a8b58c0c7e4702e20f6c
Tahmeerpasha <tahmeerpashacjc@gmail.com>
2023-08-11 23:32:21 +0530

    Added back arrow in products page

--- app/products/page.js
+++ app/products/page.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import ProductCatalog from '../../components/ProductCatalog';
 import products from '../../components/data/products'; // Create a file to store your product data
+import Link from 'next/link';
+import Image from 'next/image';
 
 
 const Home = () => {
     return (
         <div className=" w-full h-full">
+            <div className='w-full flex p-6 m-2'>
+                <Link href="../"><Image src={'/assets/icons/left-chevron.png'} alt='back arrow' height={50} width={50} /></Link>
+            </div>
             <h1 className="text-3xl lg:text-5xl p-3  text-center font-semibold mb-4"><span className='text-red-700'>Product</span> Catalog</h1>
             <ProductCatalog products={products} />
         </div>
