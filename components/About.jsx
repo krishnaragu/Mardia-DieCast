16321d5a3c63715ec3d5da7811523a14ad587c17
Tahmeerpasha <tahmeerpashacjc@gmail.com>
2023-10-13 21:35:28 +0530

    changes in content

--- components/About.jsx
+++ components/About.jsx
@@ -75,14 +75,14 @@ const About = () => {
                 </motion.p>
 
                 {/* Before and After Machining Images */}
-                <figure className='flex justify-center '>
+                <figure className='grid grid-cols-2 gap-5 justify-center '>
                     <div className=''>
-                        <Image src={"/assets/pmm/before-machining1.jpg"} width={300} height={300} alt='before-machining' className='rounded-xl ml-2 w-[35%] lg:w-fit hover:scale-105' />
-                        <p className='text-xl md:text-3xl'>Before Machining</p>
+                        <Image src={"/assets/pmm/before-machining1.jpg"} width={300} height={300} alt='before-machining' className='rounded-xl ml-2 w-fit hover:scale-105' />
+                        <p className='text-xl md:text-3xl p-5'>Before Machining</p>
                     </div>
                     <div>
-                        <Image src={"/assets/pmm/after-machining.jpg"} width={300} height={300} alt='after-machining' className='rounded-xl ml-2 w-[35%] lg:w-fit hover:scale-105' />
-                        <p className='text-xl md:text-3xl'>After Machining</p>
+                        <Image src={"/assets/pmm/after-machining.jpg"} width={300} height={300} alt='after-machining' className='rounded-xl ml-2 w-fit hover:scale-105' />
+                        <p className='text-xl md:text-3xl p-5'>After Machining</p>
                     </div>
                 </figure>
 
@@ -95,7 +95,7 @@ const About = () => {
                         With a rich experience of over
                         <span className="text-white"> 5 years</span> in the field.
                         A name synonymous with high quality & precision products
-                        that cater to <span className="text-red-800"> Engineering Industry Spare Parts</span>
+                        that cater to <span className="text-orange-500"> Engineering Industry Spare Parts</span>
                     </motion.p>
                     <motion.div
                         className="flex justify-center"
