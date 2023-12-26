8aa8fcc3ab6067b7358fa55e41a1f59049e3e613
Tahmeerpasha <tahmeerpashacjc@gmail.com>
2023-12-26 22:33:59 +0530

    Update

--- components/data/products.jsx
+++ components/data/products.jsx
@@ -50,7 +50,7 @@ const products = [
     {
         id: 0,
         name: 'FRONT NUT',
-        image: `${BASE_URL}PAGE 2/FRONT NUT.jpg`
+        image: `${BASE_URL}PAGE 3/FRONT NUT.jpg`
     },
     {
         id: 13,
