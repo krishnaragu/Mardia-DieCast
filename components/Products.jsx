1aa7df3181ce481b48d7f4f7997387d18625223b
Tahmeerpasha <tahmeerpashacjc@gmail.com>
2024-01-20 16:04:43 +0530

    Feature : Change background color

--- components/Products.jsx
+++ components/Products.jsx
@@ -23,7 +23,7 @@ const Products = () => {
             <h1 className="text-2xl text-red-700 font-bold pt-12 sm:text-5xl mb-8">
                 <span className='text-white'>OUR PRODUCTS</span>
             </h1>
-            <div className="mx-auto lg:pt-5 bg-gradient-radial from-zinc-600 to-zinc-900 p-1">
+            <div className="mx-auto lg:pt-5 bg-gradient-radial from-zinc-600 to-zinc-900 rounded-xl p-2">
                 <p className="text-orange-500 lg:p-10  text-center sm:text-lg lg:text-6xl lg:font-bold">
                     We manufacture more than <span className=''>90% of
                         mechanical spare parts required by any plant.</span> Thousands of custom-made
