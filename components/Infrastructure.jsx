a3562a9a0218c9581b50c2f61295af175bdaf925
Tahmeerpasha <tahmeerpashacjc@gmail.com>
2024-01-21 18:30:28 +0530

    Changes in Infrastructure

--- components/Infrastructure.jsx
+++ components/Infrastructure.jsx
@@ -37,6 +37,17 @@ const Infrastructure = () => {
                     initial="hidden"
                     animate="visible"
                 >
+                    <figure className='lg:flex md:grid md:grid-cols-3 justify-between'>
+                        <div className=''>
+                            <Image src={'/assets/infra/3.jpg'} alt='pic 3' className='p-10 ' width={364} height={604} />
+                        </div>
+                        <div className='lg:pt-'>
+                            <Image src={'/assets/infra/4.jpg'} alt='pic 4' className='p-10' width={372} height={581} />
+                        </div>
+                        <figure className='lg:pt0'>
+                            <Image src={'/assets/infra/2.jpg'} alt='pic 5' className='p-10' width={327} height={537} />
+                        </figure>
+                    </figure>
                     <figure className='grid lg:grid-cols-2 gap-20 p-6 justify-center items-center'>
                         <figure className=''>
                             <Image src={'/assets/infra/14.jpg'} alt='pic 5' className='rounded-xl p-1' width={1140} height={1080} />
@@ -50,26 +61,17 @@ const Infrastructure = () => {
                         <figure className=''>
                             <Image src={'/assets/infra/16.jpg'} alt='pic 5' className='rounded-xl p-1' width={1140} height={1080} />
                         </figure>
-                    </figure>
-                    <figure className='lg:flex md:grid md:grid-cols-3 justify-between'>
-                        <div className=''>
-                            <Image src={'/assets/infra/3.jpg'} alt='pic 3' className='p-10 ' width={364} height={604} />
-                        </div>
-                        <div className='lg:pt-40'>
-                            <Image src={'/assets/infra/4.jpg'} alt='pic 4' className='p-10' width={372} height={581} />
-                        </div>
-                        <figure className='lg:pt-80'>
-                            <Image src={'/assets/infra/2.jpg'} alt='pic 5' className='p-10' width={327} height={537} />
+                        <figure className=''>
+                            <Image src={'/assets/infra/6.png'} alt='pic 3' className='rounded-xl p-1' width={577} height={433} />
+                        </figure>
+                        <figure className='md:w-[50%] flex justify-center  p-5'>
+                            <Image src={'/assets/infra/13.png'} alt='pic 4' className='rounded-xl w-[90%]' width={864} height={1152} />
                         </figure>
                     </figure>
-                    <div className='lg:flex  justify-between items-center'>
-                        <figure className='lg:flex justify-between items-center'>
-                            <div className=' p-5'>
-                                <Image src={'/assets/infra/6.png'} alt='pic 3' className='rounded-xl ' width={577} height={433} />
-                            </div>
-                            <figure className='md:w-[40%] p-5'>
-                                <Image src={'/assets/infra/13.png'} alt='pic 4' className='rounded-xl' width={864} height={1152} />
-                            </figure>
+
+                    <div className='lg:flex  justify-center items-center'>
+                        <figure className='lg:grid lg:grid-cols-2 justify-between items-center'>
+
                         </figure>
                     </div>
 
